40ae4998a3bf1fcfcb9331571c4fe4a024c1eae7
freakkid <904811062@qq.com>
2017-12-11 09:28:17 +0800

    add some cod

--- service/entities/entity.go
+++ service/entities/entity.go
@@ -6,7 +6,9 @@ import (
 
 // User is an entity to save user info with username is primary key.
 type User struct {
-	UserName string `xorm:"pk varchar(255) notnull unique"`
+	ID       int    `xorm:"pk autoincr"`
+	Key      string `xorm:"varchar(255) notnull unique"`
+	UserName string `xorm:"varchar(255) notnull unique"`
 	Password string `xorm:"varchar(255) notnull"`
 	Email    string `xorm:"varchar(255) notnull"`
 	Phone    string `xorm:"varchar(255) notnull"`
@@ -17,9 +19,24 @@ type User struct {
 // the number of particulars must larger than zero,
 // time is used "*time.Time" and save in database as "YYYY-MM-DD HH:mm:SS".
 type Meeting struct {
-	Title       string
-	Sponsor     string
-	Particulars string
-	StartTime   *time.Time
-	EndTime     *time.Time
+	Title     string     `xorm:"pk varchar(255) notnull unique"`
+	Sponsor   string     `xorm:"varchar(255) notnull"`
+	StartTime *time.Time `xorm:"DateTime notnull"`
+	EndTime   *time.Time `xorm:"DateTime notnull"`
+}
+
+// Participator of meeting select by title
+type Participators struct {
+	Title        string `xorm:"varchar(255) notnull"`
+	Participator string `xorm:"varchar(255) notnull"`
+}
+
+type MeetingParticipators struct {
+	Meeting       `xorm:"extends"`
+	Participators `xorm:"extends"`
+}
+
+type UserMeeting struct {
+	User    `xorm:"extends"`
+	Meeting `xorm:"extends"`
 }
--- service/entities/initial.go
+++ service/entities/initial.go
@@ -1,10 +1,7 @@
 package entities
 
 import (
-	"database/sql"
-	"fmt"
-
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/mxk/go-sqlite/sqlite3" // as database driver of this programe
 
 	"github.com/go-xorm/core"
 	"github.com/go-xorm/xorm"
@@ -12,18 +9,6 @@ import (
 
 // ORM engine
 var xormEngine *xorm.Engine
-
-// create database
-func createDB(driverName string, createDBPara string, createDataBaseStmt string) {
-	// open a database
-	db, err := sql.Open(driverName, createDBPara)
-	defer db.Close()
-	checkErr(err)
-
-	// create database if not exist
-	_, err = db.Exec(createDataBaseStmt)
-	checkErr(err)
-}
 
 func checkErr(err error) {
 	if err != nil {
@@ -33,33 +18,13 @@ func checkErr(err error) {
 
 func init() {
 
-	const (
-		username   = "root"           // the username of mysql database
-		password   = "pincushion147"  // the password of the username
-		addrs      = "127.0.0.1"      // the tcp address
-		port       = "3306"           // the port
-		driverName = "mysql"          // name of sql driver
-		dbName     = "service_agenda" // database name
-	)
-
-	var (
-		dataSourceName = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=true",
-			username, password, addrs, port, dbName)
-		createDBPara       = fmt.Sprintf("%s:%s@tcp(%s:%s)/", username, password, addrs, port)
-		createDataBaseStmt = "CREATE DATABASE IF NOT EXISTS " + dbName
-		err                error
-	)
-
-	// create database before creating xorm engine
-	createDB(driverName, createDBPara, createDataBaseStmt)
-
+	var err error
 	// create engine
-	xormEngine, err = xorm.NewEngine(driverName, dataSourceName)
+	xormEngine, err = xorm.NewEngine("sqlite3", "./agenda.db")
 	checkErr(err)
 
 	xormEngine.SetMapper(core.GonicMapper{})
 
 	// sync the struct changes to database
-	//err = xormEngine.Sync2(new(UserInfo))
-	//checkErr(err)
+	checkErr(xormEngine.Sync2(new(User), new(Meeting), new(Participators)))
 }
