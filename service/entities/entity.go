062c44e957ccd779f0cf80788369d6e3cc69cb58
freakkid <904811062@qq.com>
2017-12-14 15:07:35 +0800

    remove useless code

--- service/entities/entity.go
+++ service/entities/entity.go
@@ -1,9 +1,5 @@
 package entities
 
-import (
-	"time"
-)
-
 // User is an entity to save user info with username is primary key.
 type User struct {
 	ID        int    `xorm:"pk autoincr"`
@@ -12,35 +8,4 @@ type User struct {
 	Password  string `xorm:"varchar(255) notnull"`
 	Email     string `xorm:"varchar(255) notnull"`
 	Phone     string `xorm:"varchar(255) notnull"`
-}
-
-// ++++++++++++++++++ the code below have not been used ++++++++++++++++++
-
-// Meeting is an entity to save meeting info with title is primary key,
-// names of particulars will created by join string array with space,
-// the number of particulars must larger than zero,
-// time is used "*time.Time" and save in database as "YYYY-MM-DD HH:mm:SS".
-type Meeting struct {
-	Title     string     `xorm:"pk varchar(255) notnull unique"`
-	Sponsor   string     `xorm:"varchar(255) notnull"`
-	StartTime *time.Time `xorm:"DateTime notnull"`
-	EndTime   *time.Time `xorm:"DateTime notnull"`
-}
-
-// Participators is Participator of meeting select by title
-type Participators struct {
-	Title        string `xorm:"varchar(255) notnull"`
-	Participator string `xorm:"varchar(255) notnull"`
-}
-
-// MeetingParticipators to join data of two tables
-type MeetingParticipators struct {
-	Meeting       `xorm:"extends"`
-	Participators `xorm:"extends"`
-}
-
-// UserMeeting to join data of two tables
-type UserMeeting struct {
-	User    `xorm:"extends"`
-	Meeting `xorm:"extends"`
 }
